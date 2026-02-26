bbad4fcf4c718477cd4665aa8c2f49592eeb9ae6
Mehrdad <mehrdads416@gmail.com>
2026-02-26 21:31:07 +0330

    cleaning up cors

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('@dotenvx/dotenvx').config();
 const express = require('express');
 const appRouter = require('./routes/appRoutes');
 const prisma = require('./db/prisma');
@@ -8,11 +7,8 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 const allowedOrigins = [
-    `${process.env.FRONTEND_URL}`,
     'http://localhost:5173',
-    'https://waldo-frontend-phi.vercel.app',
-    'https://waldo-frontend-6gafj9sha-mehrdad2929s-projects.vercel.app',
-    'https://waldo-frontend-k80otux08-mehrdad2929s-projects.vercel.app'
+    'https://waldo-frontend-phi.vercel.app'
 ];
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(cookieParser());
